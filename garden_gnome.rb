ec5fdd05cbd818b24b658dbc185c2b35686bcdc0
Sarahxdoughty <sarahxdoughty@gmail.com>
2018-07-26 19:00:34 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -11,5 +11,9 @@ class GardenGnome
   
   def shout
     return "GNARLY!!!"
+  end
+  
+  def introduction_self
+    return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
   end 
 end 
